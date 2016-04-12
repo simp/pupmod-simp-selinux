9d87d0a5de6aa07042afbd72c98bdb2dfd61bc77
Kendall Moore <kendall.moore@onyxpoint.com>
2016-04-12 16:59:47 -0400

    (SIMP-998) Remove custom type deprecation from selinux
    Updated the selinux_state custom type to remove a deprecation warning
    that occurs in Puppet 4
    
    SIMP-995 #comment Fixes the pupmod-simp-selinux part of the story
    SIMP-998 #close

--- lib/puppet/type/selinux_state.rb
+++ lib/puppet/type/selinux_state.rb
@@ -1,41 +1,39 @@
-module Puppet
-  newtype(:selinux_state) do
-    @doc = "Toggle the enforcement of selinux"
+Puppet::Type.newtype(:selinux_state) do
+  @doc = "Toggle the enforcement of selinux"
 
 
-    newparam(:name, :namevar => true) do
-      desc "An arbitrary, but unique, name for the resource."
-    end
-
-    newproperty(:ensure) do
-      defaultto(:enforcing)
-      newvalues(:false,:true,:disabled,:permissive,:enforcing)
-
-      munge do |value|
-        case value
-          when true,'true'
-            value = :enforcing
-          when false,'false'
-            value = :disabled
-        end
-
-        value
-      end
-    end
-
-    # Autorequire ALL Selbooleans
-    autorequire(:selboolean) do
-      req = []
-      resource = catalog.resources.find_all { |r|
-        r.is_a?(Puppet::Type.type(:selboolean))
-      }
-      if not resource.empty? then
-        req << resource
-      end
-      req.flatten!
-      req.each { |r| debug "Autorequiring #{r}" }
-      req
-    end
-
+  newparam(:name, :namevar => true) do
+    desc "An arbitrary, but unique, name for the resource."
   end
+
+  newproperty(:ensure) do
+    defaultto(:enforcing)
+    newvalues(:false,:true,:disabled,:permissive,:enforcing)
+
+    munge do |value|
+      case value
+        when true,'true'
+          value = :enforcing
+        when false,'false'
+          value = :disabled
+      end
+
+      value
+    end
+  end
+
+  # Autorequire ALL Selbooleans
+  autorequire(:selboolean) do
+    req = []
+    resource = catalog.resources.find_all { |r|
+      r.is_a?(Puppet::Type.type(:selboolean))
+    }
+    if not resource.empty? then
+      req << resource
+    end
+    req.flatten!
+    req.each { |r| debug "Autorequiring #{r}" }
+    req
+  end
+
 end
