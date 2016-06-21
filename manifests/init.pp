e1bafb85f02137874ec4977afcf5badc8bd1b54e
nick-markowski <nmarkowski@keywcorp.com>
2016-06-21 20:35:37 +0000

    (SIMP-1135) Install policycoreutils-python by default.
    We want it.  Some modules need it.  Let's install it.
    
    SIMP-1135 #close
    Change-Id: I71948a8138925ce0d6c1f4918d82727cb04e0cdc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,37 @@
 #
 # Class: selinux
 #
+# Parameters
+#
+# $ensure
+# Type: String
+# Default: 'enforcing'
+#   The state that SELinux should be in.
+#   Valid values are: true, false, 'enforcing', 'permissive', 'disabled'.
+#   Since you are calling this class, we assume that you want to enforce.
+#
+# $mode
+# Type: String
+# Default: 'targeted'
+#   The SELinux type you want to enforce.
+#   Valid values are: 'targeted', 'mls'
+#   Note, it is quite possible that 'mls' will render your system inoperable.
+#
+# $manage_utils_package
+# Type: Boolean
+# Default: true
+#   If true, ensure policycoreutils-python is installed.
+#
 # Additional functionality for SELinux support.
 #
 class selinux (
-# _Variables
-# $ensure
-#     The state that SELinux should be in.
-#     Valid values are: true, false, 'enforcing', 'permissive', 'disabled'.
-#     Since you are calling this class, we assume that you want to enforce.
-  $ensure = 'enforcing',
-# $mode
-#     The SELinux type you want to enforce.
-#     Valid values are: 'targeted', 'mls'
-#     Note, it is quite possible that 'mls' will render your system inoperable.
-  $mode = 'targeted'
+  $ensure               = 'enforcing',
+  $manage_utils_package = true,
+  $mode                 = 'targeted'
 ) {
   validate_array_member($mode,['targeted','mls'])
+  validate_bool($manage_utils_package)
+  validate_array_member($ensure,[true,false,'enforcing','permissive','disabled'])
 
   compliance_map()
 
@@ -28,5 +43,11 @@ class selinux (
     group   => 'root',
     mode    => '0644',
     content => template('selinux/sysconfig.erb')
+  }
+
+  if $manage_utils_package {
+    package { 'policycoreutils-python':
+      ensure => 'latest'
+    }
   }
 }
