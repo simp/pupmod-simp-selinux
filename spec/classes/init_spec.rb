fb9832b97303f4c3c2ebbc5a823221fd58c0eab7
nick-markowski <nmarkowski@keywcorp.com>
2016-06-21 20:35:37 +0000

    (SIMP-1135) Install policycoreutils-python by default.
    We want it.  Some modules need it.  Let's install it.
    
    SIMP-1135 #close
    Change-Id: I71948a8138925ce0d6c1f4918d82727cb04e0cdc

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe 'selinux' do
-  let(:facts) {{
-    :fqdn => 'test.host.net',
-    :hardwaremodel => 'x86_64',
-    :processorcount => 4,
-    :selinux => true,
-    :selinux_config_mode => 'permissive',
-    :selinux_enforced => true
-  }}
-
-  it { is_expected.to compile.with_all_deps }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+        context 'with default parameters' do
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_package('policycoreutils-python').with(:ensure => 'latest') }
+        end
+        context 'with manage_utils_package => false' do
+          let(:params) {{:manage_utils_package => false}}
+          it { is_expected.to_not contain_package('policycoreutils-python') }
+        end
+      end
+    end
+  end
 end
