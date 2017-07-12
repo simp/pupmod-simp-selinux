f9e8c60b19e73b4a46627803c8b65421ec261cf3
enemsick <lnemsick.simp@gmail.com>
2017-07-12 14:12:32 +0000

    (SIMP-3409) mcstransd service problem when selinux is disabled
    Ensure mcstrans(d) and restorecond services are not running
    when selinux is not enabled.
    
    This uses modern, selinux facts and was acceptance tested with both
    puppet 4.7.0 and puppet 4.8.2.
    
    SIMP-3409 #close

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,14 +12,43 @@
 #   If true, ensure policycoreutils-python is installed. This is a supplemental
 #   package that is required by semanage.
 #
+# @param manage_mcstrans_package 
+#   Manage the `mcstrans` package.
+#
+# @param manage_mcstrans_service
+#   Manage the `mcstrans` service.
+#
+# @param mcstrans_service_name 
+#  The `mcstrans` service name.
+#
+# @param mcstrans_package_name 
+#  The `mcstrans` package name.
+#
+# @param manage_restorecond_package 
+#   Manage the `restorecond` package.
+#
+# @param manage_restorecond_service 
+#   Manage the `restorecond` service.
+#
+# @param restorecond_package_name 
+#   The `restorecond` package name.
+#
+# @param package_ensure The ensure status of packages to be installed
+#
 class selinux (
   Selinux::State         $ensure               = simplib::lookup('simp_options::selinux', { 'default_value' => true }),
   Boolean                $manage_utils_package = true,
   String                 $package_ensure       = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' }),
-  Enum['targeted','mls'] $mode                 = 'targeted'
+  Enum['targeted','mls'] $mode                 = 'targeted',
+  # defaults are in module data
+  Boolean $manage_mcstrans_package,
+  Boolean $manage_mcstrans_service,
+  String  $mcstrans_package_name,
+  String  $mcstrans_service_name,
+  Boolean $manage_restorecond_package,
+  Boolean $manage_restorecond_service,
+  String  $restorecond_package_name,
 ) {
-
-  include 'selinux::mcstrans'
 
   selinux_state { 'set_selinux_state': ensure => $ensure }
 
@@ -43,8 +72,45 @@ class selinux (
     'checkpolicy',
     'policycoreutils-python',
   ]
+
   if $manage_utils_package {
     ensure_resource('package', $utils_packages, { 'ensure' => $package_ensure })
   }
 
+  if $manage_mcstrans_package {
+    package { $mcstrans_package_name: ensure => $package_ensure }
+  }
+
+  if $manage_restorecond_package {
+    package { $restorecond_package_name: ensure => $package_ensure }
+  }
+
+  if ($_state == 'disabled') or !$facts['os']['selinux']['enabled'] {
+    $_aux_service_ensure = 'stopped'
+  }
+  else {
+    # An ensure of 'running' requires selinux to be enabled.
+    # Final state after reboot will be correct.
+    $_aux_service_ensure = 'running'
+  }
+  
+  if $manage_mcstrans_service {
+    service { $mcstrans_service_name:
+      ensure     => $_aux_service_ensure,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => false,
+      require    => Package[$mcstrans_package_name]
+    }
+  }
+
+  if $manage_restorecond_package {
+    service { 'restorecond':
+      ensure     => $_aux_service_ensure,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => false,
+      require    => Package[$restorecond_package_name]
+    }
+  }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 describe 'selinux' do
   context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
+        #TODO add modern selinux facts to simp-rspec-puppet-facts
         let(:facts) do
-          facts
+          test_facts = os_facts.dup
+          test_facts[:os][:selinux] = {
+            :config_mode => "enforcing",
+            :config_policy => "targeted",
+            :current_mode => "enforcing",
+            :enabled => true,
+            :enforced => true,
+            :policy_version => "28"
+          }
+          test_facts
         end
 
         context 'with default parameters' do
@@ -26,6 +36,26 @@ describe 'selinux' do
           it { is_expected.to contain_package('checkpolicy').with(:ensure => 'installed') }
           it { is_expected.to contain_package('policycoreutils-python').with(:ensure => 'installed') }
           it { is_expected.to contain_reboot_notify('selinux') }
+          it { is_expected.to contain_package('mcstrans').with(:ensure => 'installed') }
+
+          if os_facts[:os][:release][:major].to_i >= 7
+            it { is_expected.to create_service('mcstransd').with({
+              :enable => true,
+              :ensure => 'running'
+            }) }
+            it { is_expected.not_to contain_package('policycoreutils-restorecond') }
+            it { is_expected.not_to create_service('restorecond') }
+          else
+            it { is_expected.to create_service('mcstrans').with({
+              :enable => true,
+              :ensure => 'running'
+            }) }
+            it { is_expected.to contain_package('policycoreutils').with(:ensure => 'installed') }
+            it { is_expected.to create_service('restorecond').with({
+              :enable => true,
+              :ensure => 'running'
+            }) }
+          end
         end
 
         context 'with ensure set to a non-boolean' do
@@ -44,7 +74,49 @@ describe 'selinux' do
             SELINUXTYPE=targeted
             EOF
             ) }
-            it { is_expected.to contain_reboot_notify('selinux') }
+          it { is_expected.to contain_reboot_notify('selinux') }
+        end
+
+        context 'with ensure set to false and restorecond enabled' do
+          let(:params) {{ 
+            :ensure => false,
+            :manage_restorecond_package => true,
+            :manage_restorecond_service => true
+          }}
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_file('/etc/selinux/config').with_content(<<-EOF.gsub(/^\s+/,'')
+            # This file controls the state of SELinux on the system.
+            # SELINUX= can take one of these three values:
+            # enforcing - SELinux security policy is enforced.
+            # permissive - SELinux prints warnings instead of enforcing.
+            # disabled - SELinux is fully disabled.
+            SELINUX=disabled
+            # SELINUXTYPE= type of policy in use. Possible values are:
+            # targeted - Only targeted network daemons are protected.
+            # strict - Full SELinux protection.
+            SELINUXTYPE=targeted
+            EOF
+            ) }
+          it { is_expected.to contain_reboot_notify('selinux') }
+          it { is_expected.to contain_package('mcstrans').with(:ensure => 'installed') }
+
+          if os_facts[:os][:release][:major].to_i >= 7
+            it { is_expected.to create_service('mcstransd').with({
+              :enable => true,
+              :ensure => 'stopped'
+            }) }
+            it { is_expected.to contain_package('policycoreutils-restorecond').with(:ensure => 'installed') }
+          else
+            it { is_expected.to create_service('mcstrans').with({
+              :enable => true,
+              :ensure => 'stopped'
+            }) }
+            it { is_expected.to contain_package('policycoreutils').with(:ensure => 'installed') }
+          end
+          it { is_expected.to create_service('restorecond').with({
+            :enable => true,
+            :ensure => 'stopped'
+          }) }
         end
 
         context 'with mode set' do
