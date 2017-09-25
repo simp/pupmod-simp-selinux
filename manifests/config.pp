9a4b1708ff1e4a2dfb4ed464a07cf9c462219093
Nick Miller <nick.miller@onyxpoint.com>
2017-09-25 17:01:48 -0400

    (SIMP-3757) selinux: Add reboot message (#30)
    SIMP-3757 #close

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,10 +3,21 @@
 class selinux::config {
   assert_private()
 
-  selinux_state { 'set_selinux_state': ensure => $::selinux::ensure }
+  selinux_state { 'set_selinux_state':
+    ensure => $::selinux::ensure
+  }
 
-  reboot_notify { 'selinux': subscribe => Selinux_state['set_selinux_state'] }
+  $_enabling  = !$facts['os']['selinux']['enabled'] and member(['enforcing','permissive'],$::selinux::state)
+  $_disabling = $facts['os']['selinux']['enabled'] and !member(['enforcing','permissive'],$::selinux::state)
 
+  if $_enabling or $_disabling {
+    reboot_notify { 'selinux':
+      reason    => 'A reboot is required to completely modify selinux state',
+      subscribe => Selinux_state['set_selinux_state']
+    }
+  }
+
+  # These vars are used in the template below
   $_state = $::selinux::state
   $_mode = $::selinux::mode
 
