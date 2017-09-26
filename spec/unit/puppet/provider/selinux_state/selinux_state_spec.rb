515f60a53aaa411048fdc5662336325ae3d74a48
lnemsick-simp <lnemsick.simp@gmail.com>
2017-09-26 17:17:54 +0000

    Try to get tests to work in travis

--- spec/unit/puppet/provider/selinux_state/selinux_state_spec.rb
+++ spec/unit/puppet/provider/selinux_state/selinux_state_spec.rb
@@ -1,76 +1,76 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:selinux_state).provider(:selinux_state) do
-
-  let(:provider) { resource.provider }
-  let(:resource) {
-    Puppet::Type.type(:selinux_state).new(
+  before :all  do
+    @resource = Puppet::Type.type(:selinux_state).new(
       name: 'set_selinux_state',
       autorelabel: true
     )
-  }
+
+    @provider = @resource.provider
+  end
 
   describe 'relabel?' do
     context 'enforcing -> enforcing' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'enforcing'
-        expect(provider.relabel?('enforcing')).to be_falsey
+        @provider.stubs(:ensure).returns 'enforcing'
+        expect(@provider.relabel?('enforcing')).to be_falsey
       end
     end
 
     context 'enforcing -> permissive' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'enforcing'
-        expect(provider.relabel?('permissive')).to be_falsey
+        @provider.stubs(:ensure).returns 'enforcing'
+        expect(@provider.relabel?('permissive')).to be_falsey
       end
     end
 
     context 'enforcing -> disabled' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'enforcing'
-        expect(provider.relabel?('disabled')).to be_falsey
+        @provider.stubs(:ensure).returns 'enforcing'
+        expect(@provider.relabel?('disabled')).to be_falsey
       end
     end
 
     context 'permissive -> enforcing' do
       it 'needs relabeling' do
-        provider.stubs(:ensure).returns 'permissive'
-        expect(provider.relabel?('enforcing')).to be_truthy
+        @provider.stubs(:ensure).returns 'permissive'
+        expect(@provider.relabel?('enforcing')).to be_truthy
       end
     end
 
     context 'permissive -> permissive' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'permissive'
-        expect(provider.relabel?('permissive')).to be_falsey
+        @provider.stubs(:ensure).returns 'permissive'
+        expect(@provider.relabel?('permissive')).to be_falsey
       end
     end
 
     context 'permissive -> disabled' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'permissive'
-        expect(provider.relabel?('disabled')).to be_falsey
+        @provider.stubs(:ensure).returns 'permissive'
+        expect(@provider.relabel?('disabled')).to be_falsey
       end
     end
 
     context 'disabled -> enforcing'do
       it 'needs relabeling' do
-        provider.stubs(:ensure).returns 'disabled'
-        expect(provider.relabel?('enforcing')).to be_truthy
+        @provider.stubs(:ensure).returns 'disabled'
+        expect(@provider.relabel?('enforcing')).to be_truthy
       end
     end
 
     context 'disabled -> permissive'do
       it 'needs relabeling' do
-        provider.stubs(:ensure).returns 'disabled'
-        expect(provider.relabel?('permissive')).to be_truthy
+        @provider.stubs(:ensure).returns 'disabled'
+        expect(@provider.relabel?('permissive')).to be_truthy
       end
     end
 
     context 'disabled -> disabled' do
       it 'does not need relabeling' do
-        provider.stubs(:ensure).returns 'disabled'
-        expect(provider.relabel?('disabled')).to be_falsey
+        @provider.stubs(:ensure).returns 'disabled'
+        expect(@provider.relabel?('disabled')).to be_falsey
       end
     end
   end
