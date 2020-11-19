4b1d8e060dc9f2e2d1e2cb7607404b240993acb6
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-11-18 20:30:59 -0500

    (SIMP-8696) Make selinux::install independent (#64)
    Fixed:
      - Allow users to include `selinux::install` without needing full
        SELinux system management. This is particularly important when the
        native types are to be used in different modules but you don't want
        to include full management just to get the required packages.
    
    SIMP-8696 #comment Make selinux::install independently usable

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,23 @@
 # @summary Install selinux-related packages not managed by vox_selinux
 #
-class selinux::install {
-  assert_private()
-
-  $utils_packages = [
-    'checkpolicy'
-  ]
-
-  if $selinux::manage_utils_package {
-    ensure_resource('package', $utils_packages, { 'ensure' => $selinux::package_ensure })
+class selinux::install (
+  Boolean       $manage_utils_package       = pick(getvar('selinux::manage_utils_package'), true),
+  Array[String] $utils_packages             = ['checkpolicy'],
+  Boolean       $manage_mcstrans_package    = simplib::lookup('selinux::manage_mcstrans_package'),
+  String        $mcstrans_package_name      = simplib::lookup('selinux::mcstrans_package_name'),
+  Boolean       $manage_restorecond_package = simplib::lookup('selinux::manage_restorecond_package'),
+  String        $restorecond_package_name   = simplib::lookup('selinux::restorecond_package_name'),
+  String        $package_ensure             = simplib::lookup('selinux::package_ensure', { 'default_value' => simplib::lookup('simp_options::package_ensure', { 'default_value' => 'present' } ) } )
+){
+  if $manage_utils_package {
+    ensure_packages($utils_packages, { 'ensure' =>  $package_ensure})
   }
 
-  if $selinux::manage_mcstrans_package {
-    package { $selinux::mcstrans_package_name: ensure => $selinux::package_ensure }
+  if $manage_mcstrans_package {
+    ensure_packages([$mcstrans_package_name], { 'ensure' =>  $package_ensure})
   }
 
-  if $selinux::manage_restorecond_package {
-    package { $selinux::restorecond_package_name: ensure => $selinux::package_ensure }
+  if $manage_restorecond_package {
+    ensure_packages([$restorecond_package_name], { 'ensure' =>  $package_ensure})
   }
 }
---
+++ spec/classes/install_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'selinux::install' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) do
+        os_facts
+      end
+
+      let(:mcstrans_service) do
+        os_facts[:os][:release][:major].to_i >= 7 ? 'mcstransd' : 'mcstrans'
+      end
+
+      let(:policycoreutils_package) do
+        os_facts[:os][:release][:major].to_i >= 7 ? 'policycoreutils-restorecond' : 'policycoreutils'
+      end
+
+      it { is_expected.to contain_package('checkpolicy').with(ensure: 'present') }
+      it { is_expected.not_to contain_package('mcstrans') }
+
+      if os_facts[:os][:release][:major].to_i >= 7
+        it { is_expected.not_to contain_package(policycoreutils_package) }
+      else
+        it { is_expected.to contain_package(policycoreutils_package).with(ensure: 'present') }
+      end
+
+      context 'when managing mcstrans' do
+        let(:params) do
+          {
+            :manage_mcstrans_package => true,
+          }
+        end
+
+        it { is_expected.to contain_package('mcstrans').with(ensure: 'present') }
+      end
+    end
+  end
+end
